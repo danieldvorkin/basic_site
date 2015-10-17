24c2f0a4f6b9871b760f290b7b0e66b81fa74252
Daniel Dvorkin <Dvorkin212@gmail.com>
2015-10-16 23:55:46 -0400

    Built out the start of the session create controller

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,4 +1,22 @@
 class SessionController < ApplicationController
   def new
+
+  end
+
+  def create
+  	# finds the user by matching the email after downcasing the entire string
+  	user = User.find_by(email: params[:session][:email].downcase)
+
+  	# if the user exist and the pw params pass the authentication process provided by has_secure_password then.....
+  	if user && user.authenticate?(params[:session][:password])
+  		# Log the user in and redirect to profile page/where ever desired
+  	else
+  		# Create and display error (Failed sign in!)
+  		render 'new'
+  	end
+  end
+
+  def destroy
+
   end
 end
