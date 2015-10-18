0af7b632bfa7f1156bed0ef7b00b11d14e386f7b
Daniel Dvorkin <Dvorkin212@gmail.com>
2015-10-18 15:34:19 -0400

    Updating master

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,9 +44,19 @@ Rails.application.configure do
 
 
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :smtp
   host = 'localhost:3000'
   config.action_mailer.default_url_options = {host: host}
+
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'localhost:3000',
+    username: ENV['GMAIL_USERNAME'],
+    password: ENV['GMAIL_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 
 
 
