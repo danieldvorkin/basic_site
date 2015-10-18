2e1af6a57fd8e14de7f9041243abff5390eb3ada
Daniel Dvorkin <Dvorkin212@gmail.com>
2015-10-18 15:24:33 -0400

    Added the activation detetection, user cannot login until account email link has been clicked and activated, still waiting for this slow ass actionmailer to send the email. Also while thinking about it, i think the smtp setup i had earlier with gmail actually worked, but not sure yet

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,11 +9,17 @@ class SessionsController < ApplicationController
 
   	# if the user exist and the pw params pass the authentication process provided by has_secure_password then.....
   	if user && user.authenticate(params[:session][:password])
-  		# Log the user in and redirect to profile page/where ever desired
-  		log_in user
-  		params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-  		redirect_back_or user
-  		flash[:success] = "Welcome Back!! " + user.name
+  		if user.activated?
+	  		# Log the user in and redirect to profile page/where ever desired
+	  		log_in user
+	  		params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+	  		redirect_back_or user
+	  	else
+	  		message = "Account not activated. "
+	  		message += "Check your email for the activation link."
+	  		flash[:warning] = message
+	  		redirect_to root_url
+  		end
   	else
   		# Create and display error (Failed sign in!)
   		flash[:danger] = "Invalid email/password combination"
