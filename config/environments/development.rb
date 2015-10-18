c7d704ba74757ed65a9a75500cf72480505ff632
Daniel Dvorkin <Dvorkin212@gmail.com>
2015-10-18 15:56:03 -0400

    Updating branch

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,14 +39,11 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-
-
-
-
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 ​
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
@@ -64,13 +61,4 @@ Rails.application.configure do
       ENV[key.to_s] = value
     end if File.exists?(env_file)
   end
-
-
-
-
-
-
-
-
-
 end
