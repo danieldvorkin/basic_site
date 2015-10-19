cf85b2736c3bd5b149832ce92c3ba9e4d1bffb3c
Daniel Dvorkin <Dvorkin212@gmail.com>
2015-10-19 15:58:42 -0400

    Updating branch

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,21 +39,7 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-​
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "gmail.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
-  }
 
   config.before_configuration do
     env_file = File.join(Rails.root, 'config', 'local_env.yml')
