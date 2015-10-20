207f1a620720c8ac43a89dc1bdbe6f82bb0716f7
Daniel Dvorkin <Dvorkin212@gmail.com>
2015-10-19 23:38:34 -0400

    moved the logged_in method from users to app controller, also built out some more fixture data and tests for both controllers and integration

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -17,13 +17,13 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 	end
 
   test "login with invalid information" do   	
-  	get login_path																				# visit the login path
-  	assert_template 'sessions/new'												# verify new session form renders properly
+  	get login_path																				 # visit the login path
+  	assert_template 'sessions/new'												 # verify new session form renders properly
   	post login_path, session: { email: "", password: "" }  # post to the session controller with invalid info
-  	assert_template 'sessions/new'												# verify new session form renders after failed auth
-  	assert_not flash.empty?																# verify flash message is NOT empty
-  	get root_path																					# visit the root path
-  	assert flash.empty?																		# verify flash message IS empty 
+  	assert_template 'sessions/new'												 # verify new session form renders after failed auth
+  	assert_not flash.empty?																 # verify flash message is NOT empty
+  	get root_path																					 # visit the root path
+  	assert flash.empty?																		 # verify flash message IS empty 
   end
 
   test "login with valid information followed by logout" do
