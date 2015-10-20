1e806c7a869906464310ebb6271914fcfa0cab49
Daniel Dvorkin <Dvorkin212@gmail.com>
2015-10-19 20:11:06 -0400

    built out the rest of the methods, along with private methods within the password_reset controller

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -21,14 +21,38 @@ class PasswordResetsController < ApplicationController
   def edit
   end
 
+  def update
+  	if params[:user][:password].empty?
+  		@user.errors.add(:password, "can't be empty")
+  		render 'edit'
+  	elsif @user.update_attribute(user_params)
+  		log_in @user
+			flash[:success] = "You've successfully changed your password, time to have fun"
+			redirect_to @user
+		else
+			render 'edit'
+  	end
+  end
+
   private
+
+  	def user_params
+  		params.require(:user).permit(:password, :password_confirmation)
+  	end
 
  		def get_user
  			@user = User.find_by(email: params[:email])
  		end
 
- 		def valid_user?
+ 		def valid_user
  			unless(@user && @user.activated? && @user.authenticated?(:reset, params[:id]))
  				redirect_to root_url
+ 		end
+
+ 		def check_expiration
+ 			if @user.password_reset_expired?
+ 				flash[:danger] = "Password Reset has expired"
+ 				redirect_to new_password_reser_url
+ 			end
  		end
 end
